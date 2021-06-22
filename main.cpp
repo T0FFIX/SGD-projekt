073616d02ce9185c15e0eab1481d384f2483bbdb
T0FFIX <adriantrapp9@gmail.com>
2021-06-22 15:01:04 -0700

    cleaned the code and added a reset function for arrow position

--- main.cpp
+++ main.cpp
@@ -34,7 +34,14 @@ const char* ARROW_PICTURE_PATH = "textures/arrow.png";
 const char* FONT_PATH = "fonts/Roboto-Thin.ttf";
 const SDL_Color Black = {0,0,0};
 
-
+void reset_arrow(SDL_Rect &arrow_position, bool &arrow_in_air, bool &arrow_crash, int &acc_x, int &acc_y)
+{
+    arrow_position  = {60, 655, 30, 15};
+    arrow_in_air = false;
+    arrow_crash = true;
+    acc_x = 0;
+    acc_y = 0;
+}
 
 int main(int, char **)
 {
@@ -83,65 +90,17 @@ int main(int, char **)
     SDL_FreeSurface(temp_arrow);
 
     milliseconds dt(15);
-    SDL_Rect target_1_position;
-    target_1_position.x = 412;
-    target_1_position.y = 600;
-    target_1_position.w = 25;
-    target_1_position.h = 25;
 
-    SDL_Rect target_2_position;
-    target_2_position.x = 500;
-    target_2_position.y = 535;
-    target_2_position.w = 25;
-    target_2_position.h = 25;
-
-    SDL_Rect target_3_position;
-    target_3_position.x = 583;
-    target_3_position.y = 470;
-    target_3_position.w = 25;
-    target_3_position.h = 25;
-
-    SDL_Rect target_4_position;
-    target_4_position.x = 675;
-    target_4_position.y = 410;
-    target_4_position.w = 25;
-    target_4_position.h = 25; 
-
-    SDL_Rect target_5_position;
-    target_5_position.x = 780;
-    target_5_position.y = 345;
-    target_5_position.w = 25;
-    target_5_position.h = 25;
-
-    SDL_Rect archer_position;
-    archer_position.x = 25;
-    archer_position.y = 630;
-    archer_position.w = 50;
-    archer_position.h = 71;
-
-    SDL_Rect arrow_position;
-    arrow_position.x = 60;
-    arrow_position.y = 655;
-    arrow_position.w = 30;
-    arrow_position.h = 15;
-
-    SDL_Rect score_position;
-    score_position.x = 750;
-    score_position.y = 0;
-    score_position.w = 40;
-    score_position.h = 50;
-    
-    SDL_Rect speed_x_position;
-    speed_x_position.x = 0;
-    speed_x_position.y = 0;
-    speed_x_position.w = 20;
-    speed_x_position.h = 25;
-
-    SDL_Rect speed_y_position;
-    speed_y_position.x = 30;
-    speed_y_position.y = 0;
-    speed_y_position.w = 20;
-    speed_y_position.h = 25;
+    SDL_Rect target_1_position = {412, 600, 25, 25};
+    SDL_Rect target_2_position = {500, 535, 25, 25};
+    SDL_Rect target_3_position = {583, 470, 25, 25};
+    SDL_Rect target_4_position = {675, 410, 25, 25};
+    SDL_Rect target_5_position = {780, 345, 25, 25};
+    SDL_Rect archer_position  = {25, 630, 50, 71};
+    SDL_Rect arrow_position  = {60, 655, 30, 15};
+    SDL_Rect score_position  = {750, 0, 40, 50};
+    SDL_Rect speed_x_position  = {0, 0, 20, 25};
+    SDL_Rect speed_y_position  = {30, 0, 20, 25};
 
     steady_clock::time_point current_time = steady_clock::now(); // remember current time
     for (bool game_active = true; game_active;)
@@ -172,7 +131,8 @@ int main(int, char **)
             }
 
             // keyboard events
-            if (event.type == SDL_KEYDOWN && arrow_in_air == false)
+            if (event.type == SDL_KEYDOWN &&
+                arrow_in_air == false)
             {
                 switch (event.key.keysym.sym)
                 {
@@ -199,80 +159,67 @@ int main(int, char **)
                 }
             }
         }
-        if (target_1_position.x <= arrow_position.x+int(30) && target_1_position.x + int(25) >= arrow_position.x+int(30) && target_1_position.y <= arrow_position.y+int(7) && target_1_position.y + int(25) >= arrow_position.y+int(7))
+        if (target_1_position.x <= arrow_position.x+int(30) &&
+            target_1_position.x + int(25) >= arrow_position.x+int(30) &&
+            target_1_position.y <= arrow_position.y+int(7) &&
+            target_1_position.y + int(25) >= arrow_position.y+int(7))
         {
             cout << "HIT" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number +int(10);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (target_2_position.x <= arrow_position.x+int(30) && target_2_position.x + int(25) >= arrow_position.x+int(30) && target_2_position.y <= arrow_position.y+int(7) && target_2_position.y + int(25) >= arrow_position.y+int(7))
+        else if (target_2_position.x <= arrow_position.x+int(30) &&
+                target_2_position.x + int(25) >= arrow_position.x+int(30) &&
+                target_2_position.y <= arrow_position.y+int(7) &&
+                target_2_position.y + int(25) >= arrow_position.y+int(7))
         {
             cout << "HIT" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number +int(30);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (target_3_position.x <= arrow_position.x+int(30) && target_3_position.x + int(25) >= arrow_position.x+int(30) && target_3_position.y <= arrow_position.y+int(7) && target_3_position.y + int(25) >= arrow_position.y+int(7))
+        else if (target_3_position.x <= arrow_position.x+int(30) &&
+                target_3_position.x + int(25) >= arrow_position.x+int(30) &&
+                target_3_position.y <= arrow_position.y+int(7) &&
+                target_3_position.y + int(25) >= arrow_position.y+int(7))
         {
             cout << "HIT" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number +int(50);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (target_4_position.x <= arrow_position.x+int(30) && target_4_position.x + int(25) >= arrow_position.x+int(30) && target_4_position.y <= arrow_position.y+int(7) && target_4_position.y + int(25) >= arrow_position.y+int(7))
+        else if (target_4_position.x <= arrow_position.x+int(30) &&
+                target_4_position.x + int(25) >= arrow_position.x+int(30) &&
+                target_4_position.y <= arrow_position.y+int(7) &&
+                target_4_position.y + int(25) >= arrow_position.y+int(7))
         {
             cout << "HIT" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number +int(70);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (target_5_position.x <= arrow_position.x+int(30) && target_5_position.x + int(25) >= arrow_position.x+int(30) && target_5_position.y <= arrow_position.y+int(7) && target_5_position.y + int(25) >= arrow_position.y+int(7))
+        else if (target_5_position.x <= arrow_position.x+int(30) &&
+                target_5_position.x + int(25) >= arrow_position.x+int(30) &&
+                target_5_position.y <= arrow_position.y+int(7) &&
+                target_5_position.y + int(25) >= arrow_position.y+int(7))
         {
             cout << "HIT" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number +int(100);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (arrow_position.x+int(30) > width+int(100) || arrow_position.y < 0 || arrow_position.y+int(15) > 700)
+        else if (arrow_position.x+int(30) > width+int(100) ||
+                arrow_position.y < 0 ||
+                arrow_position.y+int(15) > 700)
         {
             cout << "FAIL" << endl;
-            arrow_position.x = 60;
-            arrow_position.y = 655;
-            arrow_in_air = false;
-            arrow_crash = true;
             score_number = score_number - int(10);
-            acc_x = 0;
-            acc_y = 0;
+            reset_arrow(arrow_position, arrow_in_air, arrow_crash, acc_x, acc_y);
         }
-        else if (arrow_in_air == true)
+        else if (arrow_in_air)
         {
             arrow_position.x = arrow_position.x + acc_x;
             arrow_position.y = arrow_position.y + acc_y + gravity;
             gravity += 0.2;
             angle = (acc_y + gravity)*2;
         }
-        else if (arrow_crash == true)
+        else if (arrow_crash)
         {
             gravity = 5;
             angle = 0;
@@ -288,7 +235,7 @@ int main(int, char **)
             cout << "You Lose!" << endl;
             score_number = 0;
         }
-        if (arrow_in_air == false)
+        if (!arrow_in_air)
         {
             angle = (acc_y)*2;
         }
@@ -299,7 +246,6 @@ int main(int, char **)
         SDL_RenderCopy(renderer, target_picture, NULL, &target_4_position);
         SDL_RenderCopy(renderer, target_picture, NULL, &target_5_position);
         SDL_RenderCopy(renderer, archer_picture, NULL, &archer_position);
-        // SDL_RenderCopy(renderer, arrow_picture, NULL, &arrow_position);
         SDL_RenderCopyEx(renderer,arrow_picture,NULL, &arrow_position, angle, NULL, SDL_FLIP_NONE);
         SDL_RenderCopy(renderer, score_picture, NULL, &score_position);
         SDL_RenderCopy(renderer, speed_x_picture, NULL, &speed_x_position);
